48ef326bd79ec28d981abfa467309fb8ef3ecb52
jarskijr <jarskijr@rose-hulman.edu>
2019-01-31 09:32:31 -0500

    Done...ish

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -23,7 +23,7 @@ def main():
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
 
-    frame1 = ttk.Frame(root, padding=10)
+    frame1 = ttk.Frame(root, padding=20)
     frame1.grid()
 
     # -------------------------------------------------------------------------
@@ -39,20 +39,35 @@ def main():
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
     print_hello_button['command'] = (lambda: print('Hello'))
-    print_hello_button.grid()
+    print_hello_button.grid(row = 0, column = 0)
 
 
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+    sample_entry_box = ttk.Entry(frame1)
+    sample_entry_box.grid(row = 1, column = 0)
+    picky_hello_button = ttk.Button(frame1, text = 'Picky Hello')
+    picky_hello_button['command'] = (lambda: picky_hello(sample_entry_box))
+    picky_hello_button.grid(row = 2, column = 0)
+
+
+    second_entry_box = ttk.Entry(frame1)
+    second_entry_box.grid(row = 1, column = 1)
+    third_button_function = ttk.Button(frame1, text = 'Print amount of times')
+    third_button_function['command'] = (lambda: print_times(second_entry_box, sample_entry_box))
+    third_button_function.grid(row = 2, column = 1)
+
+
+
 
     # -------------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -76,12 +91,22 @@ def main():
     ####################################################################
 
     # -------------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # DONE: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
 
     root.mainloop()
 
-
+def picky_hello(input_of_picky_hello):
+    input_of_picky_hello = input_of_picky_hello.get()
+    if input_of_picky_hello == 'ok':
+        print('Hello')
+    else:
+        print('Goodbye')
+def print_times(amount_of_times, input_of_picky_hello):
+    number_of_times_input = amount_of_times.get()
+    string_to_print = input_of_picky_hello.get()
+    for k in range(int(number_of_times_input)):
+        print(string_to_print)
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
